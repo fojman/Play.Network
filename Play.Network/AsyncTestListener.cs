b2870a43a7507a4cba56320a28a5ced77484f716
yukhimoe <ievgen.iukhymovych@gmail.com>
2018-04-10 20:34:50 +0300

    Add project files.

---
+++ Play.Network/AsyncTestListener.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Play.Network
+{
+    public class AsyncTestListener
+    {
+        public async Task StartAsyncListener(ServerConfig config, CancellationToken token)
+        {
+            var listener = new TcpListener(config.Address, config.Port);
+            token.Register(listener.Stop);
+
+            listener.Start();
+
+            while (!token.IsCancellationRequested)
+            {
+                var client = await listener.AcceptTcpClientAsync()
+                    .ConfigureAwait(continueOnCapturedContext: false);
+
+                client.Client.
+            }
+        }
+    }
+}
---
+++ Play.Network/ServerConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace Play.Network
+{
+    public class ServerConfig
+    {
+        public IPAddress Address { get; set; }
+
+        public int Port { get; set; }
+    }
+}
